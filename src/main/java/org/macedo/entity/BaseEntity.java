df27f8928384769db99c2e17fed3369891d46594
ciromacedo <ciromacedo@gmail.com>
2025-03-08 17:11:27 -0300

    add new features

---
+++ src/main/java/org/macedo/entity/BaseEntity.java
@@ -0,0 +1,16 @@
+package org.macedo.entity;
+
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+
+@MappedSuperclass
+@Getter
+@Setter
+public abstract class BaseEntity implements java.io.Serializable {
+
+    @Id
+    protected Long id;
+
+}
