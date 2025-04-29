bf0460189c1b532c7b492bc7e042d19ea2fdbb21
ciromacedo <ciromacedo@gmail.com>
2025-04-29 07:40:17 -0300

    versão 1.0.10

---
+++ src/main/java/org/macedo/utils/Texto.java
@@ -0,0 +1,11 @@
+package org.macedo.utils;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Texto {
+
+    public static Boolean isNotBlank(String texto) {
+        return StringUtils.isNotBlank(texto);
+    }
+
+}
